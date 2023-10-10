4e604e6243d6aedb7bc323dfefd99367298fc460
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2023-10-10 17:13:05 +0200

    index

--- js/standing.js
+++ js/standing.js
@@ -1,11 +1,15 @@
 "use strict"
 
-import{
+import {
   standingFetch
 } from './apis.js'
 
 let teams = []
 let standing = [];
+let favorites = [];
+let leagueTitle;
+let flag;
+
 let user;
 
 const queryString = window.location.search;
@@ -13,33 +17,23 @@ const urlParams = new URLSearchParams(queryString);
 const leagueID = urlParams.get('league')
 
 
-window.onload = function getData() {
+window.onload = async function getData() {
   //check sessionStorage
- user = JSON.parse(sessionStorage.getItem('user'));
+  user = JSON.parse(sessionStorage.getItem('user'));
 
-document.getElementById('message').innerText = `Welcome ${user.username} !`
+  document.getElementById('message').innerText = `Welcome ${user.username} !`
 
   //fetch and render data
   standingFetch().then(data => {
     console.log(data)
-      teams = data.response[0].league.standings[0]
+    teams = data.response[0].league.standings[0]
+    leagueTitle = data.response[0].league.name
+    flag = data.response[0].league.flag
+    teams.forEach(s => {
+      standing.push(s)
+    });
 
-      teams.forEach(s => {
-        standing.push(s)
-      });
-
-    })
-    let leagueTitle = data.response[0].league.name
-    let flag = data.response[0].league.flag
-    let htmlString = ""
-    htmlString == `
-    <div class="comp-title">
-    <img src= ${leagueTitle} class="comp-flag">
-    <h1 class="comp-name">${leagueTitle}</h1>
-     </div>
-    `
-
-    document.getElementsByClassName('comp-title').innerHTML== htmlString
+  })
 
   setTimeout(buildList, 300)
   setTimeout(favorite, 300)
@@ -48,7 +42,14 @@ document.getElementById('message').innerText = `Welcome ${user.username} !`
 
 
 function buildList() {
+  let htmlTitle = `
+    <div class="comp-title">
+    <img src= ${flag} class="comp-flag">
+    <h1 class="comp-name">${leagueTitle}</h1>
+     </div>
+    `
 
+  document.querySelector('.comp-title').innerHTML = htmlTitle;
 
   let html = ''
   for (let s of standing) {
@@ -91,7 +92,7 @@ function buildList() {
 
 
 function infoTeam() {
-//https://www.w3schools.com/js/js_window_location.asp
+  //https://www.w3schools.com/js/js_window_location.asp
   const teams = document.getElementsByClassName("name-full");
   let teamsArray = [].slice.call(teams);
   teamsArray.forEach(team => {
@@ -107,56 +108,15 @@ function infoTeam() {
 
 
 function favorite() {
-  const buttons = document.getElementsByClassName("button");
-  let buttonsArray = Array.from(buttons);
-
-  buttonsArray.forEach((button, i) => {
-    button.addEventListener("click", function (e) {
-      let s = standing[i];
-      let teamID = s.team.id;
-      let teamName = s.team.name;
-      let teamLogo = s.team.logo;
-      let userID = user.uuid
-      if (favorite.teamID != teamID) {
-  fetch('https://web-2-host-football.onrender.com/teams', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-
-          userID: userID,
-          teamID : teamID,
-          teamName : teamName,
-          teamLogo : teamLogo
-        })
-      }).then(data => {
-        return data.json()
-      })
-      let htmlString = ""
-      htmlString += `
-
-      <div class="teams">
-        <ul>
-          <li><img src="${teamLogo}">  <a href="./teamInfo.html?id=${teamID}">${teamName}</a> </li>
-        </ul>
-      </div>`
-
-    document.getElementById('favorite').innerHTML += htmlString;
-      };
-
-    })
-  })
   let userID = user.uuid
-
   fetch(`https://web-2-host-football.onrender.com/teams/${userID}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(response => response.json())
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
     .then(data => {
-      let favorites = data;
+      favorites = data;
       let htmlString = '';
       favorites.forEach(favorite => {
         let teamID = favorite.teamID;
@@ -172,8 +132,55 @@ function favorite() {
       });
 
       document.getElementById('favorite').innerHTML += htmlString;
+      console.log(favorites);
     })
     .catch(error => console.error('Error:', error));
+  const buttons = document.getElementsByClassName("button");
+  let buttonsArray = Array.from(buttons);
+  buttonsArray.forEach((button, i) => {
+    button.addEventListener("click", function (e) {
+      let s = standing[i];
+      let teamID = s.team.id;
+      let teamName = s.team.name;
+      let teamLogo = s.team.logo;
+      let userID = user.uuid
+
+      // Check if the team is already a favorite
+      const isFavorite = favorites.some(favorite => favorite.teamID === teamID);
+
+      if (!isFavorite) {
+        fetch('https://web-2-host-football.onrender.com/teams', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+
+            userID: userID,
+            teamID: teamID,
+            teamName: teamName,
+            teamLogo: teamLogo
+          })
+        }).then(response => response.json())
+      .then(responseData => {
+        favorites.push(responseData);
+              let htmlString = ""
+        htmlString += `
+
+      <div class="teams">
+        <ul>
+          <li><img src="${teamLogo}">  <a href="./teamInfo.html?id=${teamID}">${teamName}</a> </li>
+        </ul>
+      </div>`
+
+        document.getElementById('favorite').innerHTML += htmlString;
+        window.location.reload();
+        })
+
+      }else{
+        document.getElementById('already').style.display = "flex";
+      };
+
+    })
+  })
 }
-
-
